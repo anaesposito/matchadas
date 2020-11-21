b6f8ffc47b8af8ba0f9aa2bbfda4b4625b3a702b
anaesposito <anaclaraesposito@gmail.com>
2020-11-20 23:29:26 -0300

    carpeta img para todas las imagenes

---
+++ main.js
@@ -0,0 +1,19 @@
+let imgArray = Array();
+
+imgArray[0] = Image();
+imgArray[0].src = "1.png";
+
+imgArray[1] = Image();
+imgArray[1].src = "2.png";
+
+imgArray[2] = Image();
+imgArray[2].src = "3.png";
+
+imgArray[3] = Image();
+imgArray[3].src = "4.png";
+
+imgArray[4] = Image();
+imgArray[4].src = "5.png";
+
+imgArray[5] = Image();
+imgArray[5].src = "6.png";
