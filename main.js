1389b576bbc77a480ee7224f23eb9e0077d2eb5b
anaesposito <anaclaraesposito@gmail.com>
2020-11-20 23:32:23 -0300

    probando img

---
+++ main.js
@@ -0,0 +1,21 @@
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = "img/Gatito-1.png";
+
+imgArray[1] = new Image();
+imgArray[1].src = "img/Gatito-2.png";
+
+imgArray[2] = new Image();
+imgArray[2].src = "img/Gatito-3.png";
+
+imgArray[3] = new Image();
+imgArray[3].src = "img/Gatito-4.png";
+
+imgArray[4] = new Image();
+imgArray[4].src = "img/Gatito-5.png";
+
+imgArray[5] = new Image();
+imgArray[5].src = "img/Gatito-6.png";
+
+console.log(imgArray);
